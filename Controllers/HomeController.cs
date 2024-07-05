42ffda0ce7ac05d5c1c2be6e14876d0b173aca47
Andrea Sánchez <androc3po@gmail.com>
2024-07-05 18:08:50 +0200

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Software_Veterinario.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+         
+        public ActionResult FichaMascota()
+        {
+            return View();
+        }
+       
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Software_Veterinario.MvcApplication" Language="C#" %>
